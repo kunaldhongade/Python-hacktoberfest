aa3dcc5f3c6086b47d9f120b8ca9b961d8a640d2
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-10-16 18:28:01 +0530

    Update addition.py

--- extra/addition.py
+++ extra/addition.py
@@ -1,4 +1,4 @@
-a = int(input("Enter a : "))
-b = int(input("Enter b : "))
-c = int(input("Enter c : "))
-print("Result : ", a+b+c)
+a1 = int(input("Enter a : "))
+b1 = int(input("Enter b : "))
+c1 = int(input("Enter c : "))
+print("Result : ", a1+b1+c1)
