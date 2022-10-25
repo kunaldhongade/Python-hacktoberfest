5e6ec9b20f520c1ea4e3589e3f13f5dac341be9c
Akshat Bokdia <101403537+AkshatBokdia0602@users.noreply.github.com>
2022-10-25 11:33:00 +0530

    Update 01_duplicates.py

--- 01_BasicPython/setA/01_duplicates.py
+++ 01_BasicPython/setA/01_duplicates.py
@@ -1,16 +1,11 @@
 # Write a program which acceps % integer values and prints "Duplicates"
 # if any of the values entered are duplicates otherwise it print " All Unique"
-# Example Let 5 integers are 32,45,90,45,6 then output "Duplicates " to be printed
+# Example Let 5 integers are 32,45,90,45,6 then output "Duplicates" to be printed
 
-a = [1,2,3,4,5]
-counter = 0
-print(len(a))
-for i in range(0,len(a)):
-    for j in range(i+1,len(a)):
-        if(a[i] == a[j]):
-            print("Dublicate")
-            counter+=1
-
-
-if(counter==0):
-    print("all unique")
+if __name__ == "__main__":
+    l = list(map(int,input("Enter numbers separated by space: ").split()))
+    s = set(l)
+    if len(s) == len(l):
+        print("All unique")
+    else:
+        print("Duplicates exist.\n No. of Duplicate elements = ", len(l) - len(s))
